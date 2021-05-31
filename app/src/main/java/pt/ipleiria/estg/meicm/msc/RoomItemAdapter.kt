97dfacfe51b48bca0f0c9ec8c5ca6b25a3c2c9fd
João <2202781@my.ipleiria.pt>
2021-05-31 23:02:24 +0100

    Delivery updates

--- app/src/main/java/pt/ipleiria/estg/meicm/msc/RoomItemAdapter.kt
+++ app/src/main/java/pt/ipleiria/estg/meicm/msc/RoomItemAdapter.kt
@@ -9,52 +9,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
-class RoomItemAdapter(private val dataSet: LinkedList<Room>, private val clicked: MutableLiveData<Pair<Int,Room>>) :
+class RoomItemAdapter(private val dataSet: LinkedList<Room>, private val clicked: MutableLiveData<Pair<Int, Room>>) :
         RecyclerView.Adapter<RoomItemAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val roomName: TextView = view.findViewById(R.id.lb_room_name)
         val ip: TextView = view.findViewById(R.id.lb_ip)
-
-        init {
-            // Define click listener for the ViewHolder's View.
-        }
     }
 
-    // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
                 .inflate(R.layout.room_item_recyclerview, viewGroup, false)
 
         return ViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        //viewHolder.textView.text = dataSet[position]
-
-            viewHolder.roomName.text = dataSet[position].roomName.capitalize()
-            viewHolder.ip.text = dataSet[position].ip
-            clicked.postValue(Pair(-1,dataSet[position]))
-            viewHolder.itemView.setOnClickListener {
-                viewHolder.itemView.setBackgroundColor(Color.LTGRAY)
-                clicked.postValue(Pair(position,dataSet[position]))
-            }
+        viewHolder.roomName.text = dataSet[position].roomName.capitalize(Locale.ROOT)
+        viewHolder.ip.text = dataSet[position].ip
+        clicked.postValue(Pair(-1, dataSet[position]))
+        viewHolder.itemView.setOnClickListener {
+            viewHolder.itemView.setBackgroundColor(Color.LTGRAY)
+            clicked.postValue(Pair(position, dataSet[position]))
+        }
 
 
     }
 
-
-
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
 
 }
